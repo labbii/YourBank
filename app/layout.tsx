2725433835d615e1cce538bfae90c8a3d173cf54
labii <laberion_z@hotmail.com>
2024-06-06 00:20:51 +0200

    implement the structure of folder of the app

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "YourBank",
+  description: "YourBank is a banking platform.",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
+ 
