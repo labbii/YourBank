789f94d83a325126a6ac8e4352240d2831b099c7
labbii <laberion_z@hotmail.com>
2024-08-18 18:05:15 +0200

    second push

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,36 @@
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Labbii', lastName: 'Zebica', email:'info@hotmail.com'};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+          <header className="home-header">
+            <HeaderBox 
+              type="greeting"
+              title="Welcome to YourBank"
+              user={loggedIn?.firstName || 'Guest' }
+              subtext="Manage your account and transaction from YourBank platform."
+            />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={233782.50}
+          />
+          </header>
+
+          Recent Transactions
+      </div>
+
+      <RightSidebar 
+        user = {loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
+    </section>
   )
 }
 
